407b2db59421cba69ac14302d54291f54749f121
Simon Shields <keepcalm444@gmail.com>
2017-01-14 23:57:38 +1100

    utils: add support for multiple orgs, show LegacyXperia kernels
    requested by Adrian DC
    
    Change-Id: I0b08c6b93515a222fc9ddb4bf8ef1ac2b1e336da

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def getVendorNameFromRepo(repo):
     # android_kernel_samsung_manta
     v = repo.split('_')[2]
     n = repo.split('_')[3]
-
+  print(v,n)
   return v, n
 
 def getKernelTableFromGithub():
@@ -29,17 +29,22 @@ def getKernelTableFromGithub():
   u = app.config['githubusername']
   p = app.config['githubtoken']
   g = Github(u, p)
+  orgs = ["LegacyXperia", "LineageOS"]
+  for name in orgs:
+    org = g.get_organization(name)
 
-  org = g.get_organization('LineageOS')
-
-  for repo in org.get_repos():
-    if "android_kernel_" in repo.name or "-kernel-" in repo.name:
-      if repo.name not in Kernel.objects().order_by('repo_name'):
-        v, n = getVendorNameFromRepo(repo.name)
-        if v is not "error" and n is not "error":
-          Kernel(repo_name=repo.name, last_github_update=repo.updated_at, vendor=v, device=n).save()
-          for c in CVE.objects():
-            Patches(cve=c.id, kernel=Kernel.objects.get(repo_name=repo.name).id, status=Status.objects.get(text='unpatched').id).save()
+    for repo in org.get_repos():
+      if "android_kernel_" in repo.name or "-kernel-" in repo.name:
+        repo_name = repo.name
+        if name != "LineageOS":
+          # avoid naming conflicts
+          repo_name = repo_name.replace("kernel", name + "-kernel")
+        if repo_name not in Kernel.objects().order_by('repo_name'):
+          v, n = getVendorNameFromRepo(repo.name)
+          if v is not "error" and n is not "error":
+            Kernel(repo_name=repo_name, last_github_update=repo.updated_at, vendor=v, device=n).save()
+            for c in CVE.objects():
+              Patches(cve=c.id, kernel=Kernel.objects.get(repo_name=repo_name).id, status=Status.objects.get(text='unpatched').id).save()
 
   print("Done!")
   return
