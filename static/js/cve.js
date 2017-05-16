a9ff3bb162451f5826a454d206cf4c0186d86862
Dan Pasanen <dan.pasanen@gmail.com>
2017-05-16 14:54:31 -0500

    kernel: use a jquery context menu for selecting status
    * user_friendliness++
    
    Change-Id: I039235723faee95490a5dccd8bdb100e20bfb05f

--- static/js/cve.js
+++ static/js/cve.js
@@ -1,8 +1,6 @@
-function update(c) {
-  cve_id = $(c).attr('cve_id');
-  kernel_id = $(c).attr('kernel_id');
-  oldStatus = parseInt($(c).attr('status_id'));
-  newStatus = oldStatus == 5 ? 1 : oldStatus + 1;
+function update(c, s) {
+  cve_id = c.attr('cve_id');
+  kernel_id = c.attr('kernel_id');
 
   $.ajax({
     'type': 'POST',
@@ -10,13 +8,13 @@ function update(c) {
     'contentType': 'application/json',
     'data': JSON.stringify({
              kernel_id: kernel_id,
-             status_id: newStatus,
+             status_id: s,
              cve_id: cve_id,
             })
   }).done(function(data) {
     if (data.error == "success") {
-      $(c).attr('status_id', newStatus);
-      updateCVEStatus($(c));
+      c.attr('status_id', s);
+      updateCVEStatus(c);
       $("#progressbar").attr("value", data.progress);
       $("#progressvalue").text(Math.floor(data.progress) + " %");
       updateProgressBar();
@@ -46,7 +44,29 @@ function initializeCVEStatuses() {
   });
 }
 
+function initializeContextMenus() {
+  $(function(){
+      $.contextMenu({
+          selector: '.status-context-menu',
+          trigger: 'left',
+          callback: function(key, options) {
+              if ($(this).attr('status_id') != key)
+                  update($(this), key);
+          },
+          items: {
+              // TODO: dynamically populate this
+              "1": {name: "unpatched"},
+              "2": {name: "patched"},
+              "3": {name: "does not apply"},
+              "4": {name: "waiting on upstream"},
+              "5": {name: "on gerrit"},
+          }
+      });
+  });
+}
+
 $(document).ready(function() {
   updateProgressBar();
   initializeCVEStatuses();
+  initializeContextMenus();
 });
