ea9dd5072b56adcc81ba7a30683c3bd0381ce20b
Tom Powell <zifnab@zifnab06.net>
2017-05-15 20:41:34 -0700

    PEP8 cleanup: use 4 spaces, do not mix spacing
    Change-Id: I4dfe73bd413301feb6f132aeef2836a72c0c272b

--- classes.py
+++ classes.py
@@ -3,28 +3,28 @@
 from mongoengine import *
 
 class Config(Document):
-  last_update = DateTimeField(required=True)
+    last_update = DateTimeField(required=True)
 
 class CVE(Document):
-  cve_name = StringField(required=True)
-  notes = StringField(required=False)
+    cve_name = StringField(required=True)
+    notes = StringField(required=False)
 
 class Kernel(Document):
-  repo_name = StringField(required=True)
-  last_github_update = DateTimeField(required=True)
-  vendor = StringField(required=True)
-  device = StringField(required=True)
+    repo_name = StringField(required=True)
+    last_github_update = DateTimeField(required=True)
+    vendor = StringField(required=True)
+    device = StringField(required=True)
 
 class Status(Document):
-  short_id = IntField(required=True)
-  text = StringField(required=True)
+    short_id = IntField(required=True)
+    text = StringField(required=True)
 
 class Patches(Document):
-  kernel = ObjectIdField()
-  cve = ObjectIdField()
-  status = ObjectIdField()
+    kernel = ObjectIdField()
+    cve = ObjectIdField()
+    status = ObjectIdField()
 
 class Links(Document):
-  cve_id = ObjectIdField()
-  link = StringField(required=True)
-  desc = StringField(required=False)
+    cve_id = ObjectIdField()
+    link = StringField(required=True)
+    desc = StringField(required=False)
