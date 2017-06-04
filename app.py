cd1995d0908f2808a0ace830c1af32015e80578a
Demon000 <demonsingur@gmail.com>
2017-06-04 20:07:23 +0300

    cve_tracker: materialize
    Change-Id: I981c2f157d10f7fb1c983e3c7b1a762413989b86

--- app.py
+++ app.py
@@ -116,15 +116,25 @@ def error(msg = ""):
 
 @app.route("/")
 def index():
+    vendors = {}
     kernels = Kernel.objects(deprecated__in=[False, None]).order_by('vendor', 'device')
-    return render_template('index.html', kernels=kernels, version=version, authorized=logged_in(),
+    for kernel in kernels:
+        if not kernel.vendor in vendors:
+            vendors[kernel.vendor] = []
+        vendors[kernel.vendor].append(kernel)
+    return render_template('index.html', vendors=vendors, version=version, authorized=logged_in(),
           needs_auth=app.config['GITHUB_ORG'] != 'none')
 
 @app.route("/deprecated")
 def show_deprecated():
+    vendors = {}
     kernels = Kernel.objects(deprecated=True).order_by('vendor', 'device')
-    return render_template('index.html', kernels=kernels, version=version, authorized=logged_in(),
-          needs_auth=app.config['GITHUB_ORG'] != 'none', deprecated=True)
+    for kernel in kernels:
+        if not kernel.vendor in vendors:
+            vendors[kernel.vendor] = []
+        vendors[kernel.vendor].append(kernel)
+    return render_template('deprecated.html', vendors=vendors, version=version,
+          authorized=logged_in(), needs_auth=app.config['GITHUB_ORG'] != 'none')
 
 @app.route("/<string:k>")
 def kernel(k):
@@ -142,7 +152,7 @@ def kernel(k):
     if k in devices:
         devs = devices[k]
     else:
-        devs = ['No officially supported devices!']
+        devs = []
     return render_template('kernel.html',
                            kernel = kernel,
                            cves = cves,
