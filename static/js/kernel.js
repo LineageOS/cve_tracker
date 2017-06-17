731b14d7c72b5511a2c554efa8c3702c95a01f67
Demon000 <demonsingur@gmail.com>
2017-06-17 15:42:21 +0200

    cve_tracker: materialize
    * Original work: Demon Singur <demonsingur@gmail.com>
    * Contributions by Michael W <baddaemon87@gmail.com>
    
    Change-Id: I981c2f157d10f7fb1c983e3c7b1a762413989b86

--- static/js/kernel.js
+++ static/js/kernel.js
@@ -1,127 +1,219 @@
-function openLinks(cve, cve_id) {
-  $("#cvelinks").text("Loading...");
-  $("#cvenotes").text("Loading...");
+(function() {
+	function deprecateKernel(button) {
+		var d = this;
+		var kernelId = d.element.getAttribute('kernel_id');
+		var deprecated = d.element.getAttribute('deprecated');
 
-  $.ajax({
-    'type': 'POST',
-    'url': '/getlinks',
-    'contentType': 'application/json',
-    'data': JSON.stringify({
-             cve_id: cve_id,
-            })
-  }).done(function(data) {
-    $("#cvelinks").empty();
-    $.each(JSON.parse(data), function(i, v) {
-      if (!v.desc) v.desc = 'No description';
-      $("#cvelinks").append("<a href='" + v.link + "' target='_blank'>" + shorten(v.link, 80) + "</a> - " + v.desc + "<hr>");
-    });
-    $("#cveeditlink").attr("href", "/editcve/" + cve);
-    $("#cvecomparelink").attr("href", "/status/" + cve);
-    $("#cveinfodialog").dialog('close');
-    $("#cveinfodialog").dialog('option', 'title', cve).dialog('open');
-  });
+		$.ajax({
+			type: 'POST',
+			url: '/deprecate',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				kernel_id: kernelId,
+				deprecate: deprecated,
+			})
+		}).done(function(data) {
+			location.reload();
+		});
+	}
 
-  $.ajax({
-    'type': 'POST',
-    'url': '/getnotes',
-    'contentType': 'application/json',
-    'data': JSON.stringify({
-      cve_id: cve_id,
-    })
-  }).done(function(data) {
-    data = JSON.parse(data);
-    $('#cvenotes_input').val(data[0].notes);
-    if (!data[0].notes) {
-      data[0].notes = 'No notes';
-    }
-    $("#cvenotes").text(data[0].notes);
-    $('#editnotesdialog').attr('cve_id', cve_id);
-  });
-}
+	var deprecateKernelDialog = new Dialog({
+		element: document.querySelector('#deprecate-kernel-dialog'),
+		drag: '.title',
+		actions: [{
+			callback: 'close',
+			selector: '.actions .cancel'
+		}, {
+			callback: deprecateKernel,
+			selector: '.actions .mark'
+		}],
+		trigger: document.querySelector('#open-deprecate-kernel-dialog')
+	});
 
-$(document).ready(function() {
-  $("#cveinfodialog").dialog({autoOpen: false, width: 'auto' });
-  $("#cveinfodialog").on('dialogbeforeclose', function(event, ui) {
-    $("#editnotesdialog").dialog('close');
-  });
+	var CVEInfoDialog = new Dialog({
+		element: document.querySelector('#cve-info-dialog'),
+		drag: '.title',
+		actions: [{
+			callback: editCVE,
+			selector: '.actions .edit'
+		}, {
+			callback: compareCVE,
+			selector: '.actions .compare'
+		}, {
+			callback: cancelCVEInfoDialog,
+			selector: '.actions .cancel'
+		}, {
+			id: 'save',
+			callback: saveNotes,
+			selector: '.actions .save'
+		}],
+		access: {
+			title: '.title',
+			editNotesIcon: '.notes .icon',
+			notesField: '.notes .field',
+			links: '.links',
+			error: '.error'
+		}
+	});
+	CVEInfoDialog.access.editNotesIcon.addEventListener("click", editNotes);
 
-  $("#editnotesdialog").dialog({
-    autoOpen: false,
-    width: 'auto',
-    buttons: [
-      {
-        text: "Save!",
-        id: "savenotes",
-        click: savenotes
-      }
-    ]
-  });
+	function editCVE(button) {
+		var d = this;
+		var cve_name = d.element.getAttribute('cve_name');
+		window.location = "/editcve/" + cve_name;
+	}
 
-  $("#deprecationdialog").dialog({
-    autoOpen: false,
-    width: "auto",
-    modal: true,
-    buttons: [
-      {
-        text: "Do eet!",
-        click: savedeprecation
-      }
-    ]
-  });
-});
+	function compareCVE(button) {
+		var d = this;
+		var cve_name = d.element.getAttribute('cve_name');
+		window.location = "/status/" + cve_name;
+	}
 
-function editnotes() {
-  $('#editnotesdialog').dialog('open');
-}
+	function cancelCVEInfoDialog(button) {
+		var d = this;
+		var editing = CVEInfoDialog.access.notesField.getAttribute("contenteditable");
+		if (editing == "true") {
+			getNotes();
+		} else {
+			d.close();
+		}
+	}
 
-function savenotes() {
-  var cve_id = $('#editnotesdialog').attr('cve_id');
-  var notes = $('#cvenotes_input').val();
-  $.ajax({
-    'type': 'POST',
-    'url': '/editnotes',
-    'contentType': 'application/json',
-    'data': JSON.stringify({
-             cve_id: cve_id,
-             cve_notes: notes,
-    })
-  }).done(function(data) {
-    if (data.error == "success") {
-      if (!notes) {
-        notes = 'No notes';
-      }
-      $('#cvenotes').text(notes);
-      $('#editnotesdialog').dialog('close');
-    } else {
-        $("#editnoteserror").empty().append(data.error);
-    }
-  });
-}
+	function openLinks(cve_name, cve_id) {
+		CVEInfoDialog.access.title.innerHTML = cve_name;
+		CVEInfoDialog.access.notesField.innerHTML = "Loading ...";
+		CVEInfoDialog.access.links.innerHTML = "Loading ...";
+		CVEInfoDialog.element.setAttribute("cve_name", cve_name);
+		CVEInfoDialog.element.setAttribute("cve_id", cve_id);
+		restoreNotesEditable();
+		CVEInfoDialog.open();
 
-function deprecate() {
-  $('#deprecationdialog').dialog('open');
-}
+		getNotes(cve_id);
+		getLinks(cve_id);
+	}
+	var cves = [].slice.call(document.querySelectorAll('.cve .name'));
+	cves.forEach(function(cve) {
+		var name = cve.getAttribute('cve_name');
+		var id = cve.getAttribute('cve_id');
+		cve.addEventListener('click', function() {
+			openLinks(name, id);
+		});
+	});
 
-function savedeprecation() {
-  var kernel_id = $( this ).attr('kernel_id');
-  var deprecated = $( this ).attr('deprecated')
-  $.ajax({
-    'type': 'POST',
-    'url': '/deprecate',
-    'contentType': 'application/json',
-    'data': JSON.stringify({
-            kernel_id: kernel_id,
-            deprecate: deprecated,
-    })
-  }).done(function(data) {
-    location.reload();
-  });
-}
+	function getNotes(cve_id) {
+		restoreNotesEditable();
+		$.ajax({
+			'type': 'POST',
+			'url': '/getnotes',
+			'contentType': 'application/json',
+			'data': JSON.stringify({
+				cve_id: cve_id,
+			})
+		}).done(function(data) {
+			data = JSON.parse(data);
+			$('#cvenotes_input').val(data[0].notes);
+			if (!data[0].notes) {
+				data[0].notes = 'No notes';
+			}
+			CVEInfoDialog.access.notesField.innerHTML = data[0].notes;
+		});
+	}
 
-function shorten(text, maxLength) {
-  var ret = text;
-  if (ret.length > maxLength) {
-    ret = ret.substr(0, maxLength - 3) + "...";
-  }
-  return ret;
-}
+	function getLinks(cve_id) {
+		$.ajax({
+			'type': 'POST',
+			'url': '/getlinks',
+			'contentType': 'application/json',
+			'data': JSON.stringify({
+				cve_id: cve_id
+			})
+		}).done(function(data) {
+			var linkList = CVEInfoDialog.access.links;
+			linkList.innerHTML = "";
+			if (data.length) {
+				var links = JSON.parse(data);
+				links.forEach(function(v) {
+					var description = v.desc;
+					var url = v.link;
+					var id = v._id.$oid;
+					if (!description) {
+						description = 'No description';
+					}
+
+					var linkItem = createElement('div', {
+						parent: linkList
+					});
+
+					createElement('a', {
+						class: 'link',
+						href: url,
+						content: shorten(url, 80),
+						parent: linkItem
+					});
+
+					createElement('span', {
+						class: 'linkdesc',
+						content: ' - ' + description,
+						parent: linkItem
+					});
+				});
+			} else {
+				linkList.innerHTML = "No links available";
+			}
+		});
+	}
+
+	function editNotes() {
+		CVEInfoDialog.access.notesField.setAttribute("contenteditable", true);
+		CVEInfoDialog.access.editNotesIcon.classList.remove("mdi-pencil");
+		CVEInfoDialog.actions.save.disabled = false;
+	}
+
+	function saveNotes() {
+		var cve_id = CVEInfoDialog.element.getAttribute('cve_id');
+		var notes = CVEInfoDialog.access.notesField.innerHTML;
+
+		$.ajax({
+			'type': 'POST',
+			'url': '/editnotes',
+			'contentType': 'application/json',
+			'data': JSON.stringify({
+				cve_id: cve_id,
+				cve_notes: notes,
+			})
+		}).done(function(data) {
+			if (data.error == "success") {
+				if (!notes) notes = 'No notes';
+				CVEInfoDialog.access.error.innerHTML = "";
+				CVEInfoDialog.access.notesField.innerHTML = notes;
+				restoreNotesEditable();
+			} else {
+				CVEInfoDialog.access.error.innerHTML = data.error;
+			}
+		});
+	}
+
+	function restoreNotesEditable() {
+		CVEInfoDialog.access.editNotesIcon.classList.remove("mdi-pencil");
+		CVEInfoDialog.access.editNotesIcon.classList.add("mdi-pencil");
+		CVEInfoDialog.access.notesField.setAttribute("contenteditable", false);
+		CVEInfoDialog.access.error.innerHTML = "";
+		CVEInfoDialog.actions.save.disabled = true
+	}
+
+	function shorten(text, maxLength) {
+		var ret = text;
+		if (ret.length > maxLength) {
+			ret = ret.substr(0, maxLength - 3) + "...";
+		}
+		return ret;
+	}
+
+	var progressBar = new Progress({
+		container: document.querySelector('#progress-bar'),
+		element: document.querySelector('#progress-bar-inner'),
+		valueField: document.querySelector('#progress-value'),
+		value: document.querySelector('#progress-bar').getAttribute('value')
+	});
+	window.progressBar = progressBar;
+})();
