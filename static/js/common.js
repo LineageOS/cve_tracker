b4c2ede2b1c2d95f5d6c6587968ea41e1a9c068e
Demon000 <demonsingur@gmail.com>
2017-06-22 16:10:00 +0300

    cve_tracker: remove footer force positioning
    It's buggy, and on the official tracker there aren't any short pages.
    
    Change-Id: I965d80ae9a8f6d79e6f19c845e52564a11441b7b

--- static/js/common.js
+++ static/js/common.js
@@ -1,26 +1,6 @@
 (function() {
     Ripple(['#navbar .logo', '#navbar .items > *', 'button']);
 
-    var footer = document.querySelector('#footer');
-    var footerIsFixed = false;
-
-    function toggleFixedFooter() {
-        footer.classList.toggle('fixed');
-        footerIsFixed = !footerIsFixed;
-    }
-
-    function checkFixedFooter() {
-        var footerBoundingRect = footer.getBoundingClientRect();
-        if (footerBoundingRect.bottom < window.innerHeight) {
-            toggleFixedFooter();
-        } else if (footerIsFixed){
-            toggleFixedFooter();
-            checkFixedFooter();
-        }
-    }
-    window.addEventListener('resize', checkFixedFooter);
-    checkFixedFooter();
-
     var themes = {
         light: '/static/css/light.css',
         dark: '/static/css/dark.css'
