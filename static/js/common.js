8292c014082fe911b99b6e1a1e2d537a6e58a509
Demon000 <demonsingur@gmail.com>
2017-06-22 16:06:37 +0300

    cve_tracker: add dark theme
    * move coloring from style.css to light.css
    * remove some unused styles
    * add dark.css
    * add Theme Engine (hehe)
    * add a select in the footer for selecting the theme
    
    Change-Id: I36bb7d9b8fabf26bc5e869e2b0891a65a29ce9fe

--- static/js/common.js
+++ static/js/common.js
@@ -20,4 +20,32 @@
     }
     window.addEventListener('resize', checkFixedFooter);
     checkFixedFooter();
+
+    var themes = {
+        light: '/static/css/light.css',
+        dark: '/static/css/dark.css'
+    };
+    var defaultTheme = 'light';
+
+    var themeEngine = new ThemeEngine({
+        target: document.querySelector('#theme-engine-target'),
+        default: defaultTheme,
+        themes: themes
+    });
+
+    var setTheme = themeEngine.get();
+    var themeSelector = document.querySelector('#theme-selector');
+    Object.keys(themes).forEach(function(name) {
+        var themeOption = createElement('option', {
+            content: name,
+            value: name,
+            parent: themeSelector
+        });
+        if (themes[name] == setTheme) {
+            themeOption.selected = true;
+        }
+    });
+    themeSelector.onchange = function(e) {
+        themeEngine.set(themeSelector.value);
+    }
 })();
