bd34b7a21e5417d0ec063c772fe7e588d8c1b61a
Demon000 <demonsingur@gmail.com>
2017-06-23 18:54:59 +0300

    cve_tracker: add dark theme
    * remove some unused styles
    * move coloring from style.css to light.css
    * add dark.css
    * add Theme Switcher
    * add a select in the footer for switching the theme
    
    Change-Id: I36bb7d9b8fabf26bc5e869e2b0891a65a29ce9fe

--- static/js/common.js
+++ static/js/common.js
@@ -20,4 +20,36 @@
     }
     window.addEventListener('resize', checkFixedFooter);
     checkFixedFooter();
+
+    var themes = {
+        light: '/static/css/light.css',
+        dark: '/static/css/dark.css'
+    };
+    var defaultTheme = 'light';
+
+    var themeSwitcher = new ThemeSwitcher({
+        target: document.querySelector('#theme-target'),
+        default: defaultTheme,
+        themes: themes
+    });
+
+    function setTheme(from, value) {
+        themeSwitcher.set(value);
+        from.innerHTML = toTitleCase(value);
+    }
+    var themeMenuSelector = '#theme-menu';
+    var themeMenuItems = Object.keys(themes).map(function(i) {
+        return {
+            value: i,
+            text: i
+        };
+    });
+    var themeMenu = new ContextMenu({
+        selector: themeMenuSelector,
+        trigger: 'click',
+        callback: setTheme,
+        items: themeMenuItems
+    });
+    var themeMenuElement = document.querySelector(themeMenuSelector);
+    setTheme(themeMenuElement, themeSwitcher.get());
 })();
