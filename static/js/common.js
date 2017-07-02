c3d73c9d927f539538864816a0c362a05fcf92cc
Demon000 <demonsingur@gmail.com>
2017-07-02 13:49:56 +0200

    cve_tracker: fix footer force positioning
    JavaScript-only solution didn't work right, so simplify approach
    and add a HTML wrapper and some CSS.
    
    Change-Id: I965d80ae9a8f6d79e6f19c845e52564a11441b7b

--- static/js/common.js
+++ static/js/common.js
@@ -1,26 +1,6 @@
 (function() {
     Ripple(['#navbar .logo', '#navbar .items > *', 'button']);
 
-    var footer = document.querySelector('#footer');
-    var footerIsFixed = false;
-
-    function toggleFixedFooter() {
-        footer.classList.toggle('fixed');
-        footerIsFixed = !footerIsFixed;
-    }
-
-    function checkFixedFooter() {
-        var footerBoundingRect = footer.getBoundingClientRect();
-        if (footerBoundingRect.bottom < window.innerHeight) {
-            toggleFixedFooter();
-        } else if (footerIsFixed){
-            toggleFixedFooter();
-            checkFixedFooter();
-        }
-    }
-    window.addEventListener('resize', checkFixedFooter);
-    checkFixedFooter();
-
     var themes = {
         light: '/static/css/light.css',
         dark: '/static/css/dark.css'
@@ -57,4 +37,13 @@
     });
     var themeMenuElement = document.querySelector(themeMenuSelector);
     setTheme(themeMenuElement, themeSwitcher.get());
+
+
+    var footer = document.querySelector('#footer');
+    var wrapper = document.querySelector('#wrapper');
+    function padWrapper() {
+        wrapper.style.paddingBottom = footer.offsetHeight + 'px';
+    }
+    window.addEventListener('resize', padWrapper);
+    setTimeout(padWrapper, 100);
 })();
