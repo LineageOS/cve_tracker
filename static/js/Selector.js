d6b2e945c5910a129bc96853f0a0f4904fdb9181
Michael W <baddaemon87@gmail.com>
2017-08-01 18:55:08 +0200

    cve_tracker: Add tags for CVEs
    Change-Id: Id58a6b1b9e478294a59a82f2c84ada15be7bb88d

---
+++ static/js/Selector.js
@@ -0,0 +1,93 @@
+(function() {
+	function isDefined(value) {
+		return typeof value != 'undefined';
+	}
+
+	function Selector(o) {
+		var s = this;
+
+		if (typeof Emitter != 'undefined') {
+			Emitter.mixin(s);
+		}
+
+		var options = {};
+
+		s.isActive = function(op) {
+			return options[op].classList.contains('active');
+		};
+
+		s.isOption = function(op) {
+			return isDefined(options[op]);
+		};
+
+		function _getActive(multiple) {
+			var a = [];
+			for (var i in options) {
+				if (s.isActive(i)) {
+					a.push(i);
+				}
+			}
+			return multiple ? a : a[0];
+		}
+
+		s.getActive = function(asArray) {
+			var multiple = isDefined(asArray) ? asArray : o.multiple;
+			return _getActive(multiple);
+		};
+
+		function _setActive(op, value) {
+			if (!s.isOption(op) || value == s.isActive(op)) {
+				return;
+			}
+
+			if (value) {
+				options[op].classList.add('active');
+			} else {
+				options[op].classList.remove('active');
+			}
+
+			if (typeof Emitter != 'undefined') {
+				s.emit('activeChange', op, value);
+			}
+		}
+
+		s.setActive = function(op, value) {
+			if (o.multiple) {
+				_setActive(op, value);
+			} else {
+				if (value) {
+					_setActive(s.getActive(false), false);
+					_setActive(op, true);
+				}
+			}
+		};
+
+		s.toggleActive = function(op) {
+			s.setActive(op, !s.isActive(op))
+		};
+
+		s.addOption = function(op, el) {
+			el.addEventListener('click', function() {
+				s.toggleActive(op);
+			});
+			options[op] = el;
+		};
+
+		s.getOptions = function() {
+			return options;
+		};
+
+		s.setOptions = function(newOptions) {
+			options = {};
+			for (var i in newOptions) {
+				s.addOption(i, newOptions[i]);
+			}
+		};
+
+		if (o.options) {
+			s.setOptions(o.options);
+		}
+	}
+
+	window.Selector = Selector;
+})();
