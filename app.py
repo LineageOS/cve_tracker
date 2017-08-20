b88cb66c3be7d7ddce5d3707f6ca1ba32a001a92
Harry Youd <harry@harryyoud.co.uk>
2017-08-20 17:48:45 +0100

    Specify previous value in log
    Change-Id: I1ac76c1c0abacaa557b11e56367aae3b91f86c2b

--- app.py
+++ app.py
@@ -322,12 +322,13 @@ def update():
     c = r['cve_id'];
     s = r['status_id'];
 
+    old_status = Status.objects.get(id=Patches.objects.get(kernel=k, cve=c)['status'])
     status = Status.objects.get(short_id=s)
     Patches.objects(kernel=k, cve=c).update(status=status.id)
     progress = utils.getProgress(k)
     Kernel.objects(id=k).update(progress=progress)
     cveName = CVE.objects.get(id=c)['cve_name']
-    writeLog("patched", k, cveName + ": " + status.text)
+    writeLog("patched", k, cveName + ": From " + old_status.text + " to " + status.text)
     return jsonify({'error': 'success', 'progress': progress})
 
 
