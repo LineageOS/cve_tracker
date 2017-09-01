f607ded4d8388213ab9f23a3512a8dce01325609
Tom Powell <zifnab@zifnab06.net>
2017-08-31 19:10:46 -0700

    use a sane API
    Change-Id: I61f4fa5b950f0263c5745643ec7e41a1aea0251e

--- cvescore.py
+++ cvescore.py
@@ -1,58 +1,20 @@
 #!/usr/bin/python3
 
 import sys
+import requests
 
-try:
-    # For python3
-    from urllib.request import Request, urlopen
-    from html.parser import HTMLParser
-except ImportError:
-    # For python2
-    from urllib2 import Request, urlopen
-    from HTMLParser import HTMLParser
-
-
-class LinksParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.recording = 0
-        self.data = 0
-
-    def handle_starttag(self, tag, attributes):
-        if tag != 'div':
-            return
-        if self.recording:
-            self.recording += 1
-            return
-        for name, value in attributes:
-            if name == 'class' and value == 'cvssbox':
-                break
-        else:
-            return
-        self.recording = 1
-
-    def handle_endtag(self, tag):
-        if tag == 'div' and self.recording:
-            self.recording -= 1
-
-    def handle_data(self, data):
-        if self.recording:
-            self.data = data
 
 def get_score(cve):
     if cve[:3] == 'LVT':
         return 10.0
     score = -1
     try:
-        p = LinksParser()
-        url = "https://www.cvedetails.com/cve/" + cve
-        hdr = {'User-Agent': 'Mozilla/5.0'} # required to not get a http error 403
-        req = Request(url, headers=hdr)
-        html = urlopen(req).read().decode('utf-8')
-        p.feed(html)
-        score = p.data
-        p.close()
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
+        response = requests.get("https://cve.circl.lu/api/cve/{}".format(cve))
+        response.raise_for_status()
+        score = response.json()['cvss']
+        if not score:
+            score = -1
+    except Exception as e:
+        print(e)
 
     return score
