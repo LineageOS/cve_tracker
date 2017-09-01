9ef728d8a7677ed30625b1320a2e5f04fa85d70a
Michael W <baddaemon87@gmail.com>
2017-09-01 10:06:42 +0200

    cve_tracker: Scores should always be valid
    * Allow saving a score of "0" because otheriwse you can't change notes or
      tags without touching the scores as well
    * Init all CVEs which don't have an official score with 0 instead of -1
      so they are valid in the first place
    
    Change-Id: I46299648b1ac4a643471818f40a8b0e6e57ffcc2

--- cvescore.py
+++ cvescore.py
@@ -5,13 +5,13 @@ import requests
 def get_score(cve):
     if cve[:3] == 'LVT':
         return 10.0
-    score = -1
+    score = 0
     try:
         response = requests.get("https://cve.circl.lu/api/cve/{}".format(cve))
         response.raise_for_status()
         score = response.json()['cvss']
         if not score:
-            score = -1
+            score = 0
     except Exception as e:
         print(e)
 
