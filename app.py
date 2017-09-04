6dd65fee628ae48a85ed660570fe611a62b8b067
Michael W <baddaemon87@gmail.com>
2017-09-04 10:45:40 +0200

    cve_tracker: add a way to overwrite autogenerated kernels mapping
    Change-Id: I4d408736c43e4ffd893630902c4949ce0e2ba25e

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ from flask import Flask, abort, jsonify, redirect, render_template, request, ses
 from flask_github import GitHub
 from flask_mongoengine import MongoEngine
 
-devicefile = "kernels.json"
+mappingFile = "kernels.json"
+mappingOverrideFile = "kernels_override.json"
 forceDBUpdate = False
 
 version = subprocess.check_output(["git", "describe", "--always"],
@@ -37,8 +38,21 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 dir = os.path.dirname(__file__)
 
-with open(os.path.join(dir, devicefile)) as device_file:
-    devices = json.load(device_file)
+with open(os.path.join(dir, mappingFile)) as mappingRaw:
+    devices = json.load(mappingRaw)
+
+with open(os.path.join(dir, mappingOverrideFile)) as mappingOverrideRaw:
+    devicesOverride = json.load(mappingOverrideRaw)
+    for kernel in devicesOverride:
+        if not kernel in devices:
+            devices.setdefault(kernel, [])
+        for dev in devicesOverride[kernel]:
+            op = dev[:1]
+            device = dev[1:]
+            if op == '+':
+                devices[kernel].append(device)
+            elif op == '-':
+                devices[kernel].remove(device)
 
 db = MongoEngine(app)
 github = GitHub(app)
